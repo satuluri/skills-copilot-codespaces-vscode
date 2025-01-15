4697cfd5b602c026a26be5d313c8db021ae40f77
satuluri <40626708+satuluri@users.noreply.github.com>
2025-01-15 19:24:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,27 @@
+// Create a Web Server code that will handle the following requests:
+// GET /comments - returns a list of comments
+// POST /comments - create a new comment
+// PUT /comments/:id - update a comment
+// DELETE /comments/:id - delete a comment
+// The comments should be stored in a file called comments.json
+// The comments should have the following structure:
+// {
+//     "id": 1,
+//     "body": "comment body",
+//     "postId": 1
+
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+
+app.get('/comments', (req, res) => {
+    fs.readFile('comments.json', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading file');
+        } else {
+            res.send(JSON.parse(data));
+        }
+    });
+}
